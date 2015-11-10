2f2905ee7f2e4d48bf002821ca36426d2128193a
Grant James <grant.james@cardinalhealth.com>
2015-11-10 17:23:26 -0500

    quick commit

--- nit.js
+++ nit.js
@@ -282,7 +282,7 @@ function Nit() {
             self.git(["pull", "origin", "develop"], function(){
                 self.git(["checkout", currentBranch], function(data){
                     self.git(["merge", "develop"], function(data){
-                        self.printer.printer(data);
+                        self.printer.print(data);
                     });
                 });
             });
