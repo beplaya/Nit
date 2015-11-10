6888ae89ecfb1d967cb32ffbc5c0537134bf77c2
Grant James <grant.james@cardinalhealth.com>
2015-11-10 16:17:33 -0500

    NIT-fb n

--- nit.js
+++ nit.js
@@ -459,7 +459,7 @@ function Nit() {
     this.run = function(cmd, cmdArgs, cb) {
         var spawn = require('child_process').spawn,
         ls = spawn(cmd, cmdArgs);
-        ///console.log("RUNNING ", cmd, cmdArgs);
+        // console.log("RUNNING ", cmd, cmdArgs);
         var ran = false;
         ls.stdout.on('data', function (data) {
             ran = true;
