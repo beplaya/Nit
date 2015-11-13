4a624fc4747d6c0a226021f919162fb857526851
Grant James <grant.james@cardinalhealth.com>
2015-11-13 10:42:10 -0500

    NIT-nira max line lenght

--- nit.js
+++ nit.js
@@ -66,6 +66,7 @@ function Printer(){
     this.UNTRACKED = "UNTRACKED";
     this.margin = "                 ";
     this.smallMargin = "          ";
+    this.maxLineLength = 80;
 
     this.colors.setTheme({
       silly: 'rainbow',
@@ -108,6 +109,7 @@ function Printer(){
     };
 
     this._description = function(key, description, status, issuetype, assignee) {
+        description = this.truncateLines(description);
         console.log(("\t|  " + key).title);
         // console.log("\t| ".title, "Status:".title, status, "\tType:".title, issuetype, "\tAssignee:".title,  assignee);
         this.hr("\t|");
@@ -127,11 +129,38 @@ function Printer(){
                 var author =c.author.displayName;
                 console.log("\t#".verbose);
                 console.log("\t#".verbose, "author: ", author.name, "\t created: ", c.created.verbose, "\tupdated: "+c.updated.verbose);
-                self.printLinesWithPrefix("\t# \t", c.body);
+                var body = self.truncateLines(c.body);
+                self.printLinesWithPrefix("\t#   ", body);
             }
         }else {
             console.log("No comments");
         }
+    };
+
+    this.truncateLines = function(str) {
+        var max = this.maxLineLength;
+        var truncated = "";
+        var strings = str.split("\n");
+        for(var i =0; i<strings.length; i++) {
+            var s = strings[i];
+            if(s.length > max) {
+                var words = s.split(" ");
+                var line = "\n";
+
+                for(var j = 0; j<words.length; j++) {
+                   line += " "+words[j];
+                   var next = line + (j<words.length-1 ? " " + words[j+1] : "");
+                   if(next.length >= max){
+                        truncated += line;
+                        line = "\n";
+                   }
+                }
+                truncated += line === "\n" ? "" : line;
+            } else {
+                truncated += "\n" + s;
+            }
+        }
+        return truncated;
     };
 
     this.printLinesWithPrefix = function(margin, str) {
