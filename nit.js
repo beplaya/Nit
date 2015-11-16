77f42682f1ef6b25dea790f0728ce47bd5a183e1
Grant James <grant.james@cardinalhealth.com>
2015-11-16 17:23:26 -0500

    ads asd

--- nit.js
+++ nit.js
@@ -58,6 +58,7 @@ function Nit() {
                        });
                    }
                },
+               
                {arg: "nerver", name: "Start nerver", requiresClean: false, action: function(nit, arg, currentBranch){ nit.startNerver(); }},
                {arg: "browse", name: "Browse jira", requiresClean: false, action: function(nit, arg, currentBranch){ nit.browse(currentBranch); }},
                {arg: "describe", name: "Get JIRA description", requiresClean: false, action: function(nit, arg, currentBranch){ nit.describe(currentBranch); }},
