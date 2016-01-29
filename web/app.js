1c645297bb9d29b5f8e4bcea2948f1087f05a925
Grant James <grant.james@cardinalhealth.com>
2016-01-29 11:51:34 -0500

    NIT-2 wip created rest path for project status and logs

--- web/app.js
+++ web/app.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var app = express();
 var port = '9000';
+var fs = require('fs');
 
 app.listen(port);
 
 app.get('/test', function(req, res){
 	res.send('it works');
+});
+
+
+app.get('/projects/:project_key/:which_data', function(req, res){
+    try {
+        var projectKey = req.params.project_key.toUpperCase();
+        var whichData = req.params.which_data.toLowerCase();
+        var file = __dirname+"/project_cache/project_"+projectKey+"/json/"+whichData+".json";
+        var contents = fs.readFileSync(file, "utf8");
+        contents = JSON.parse(contents);
+        res.status(200).json(contents);
+	}catch(e){
+	    console.log(e);
+	    res.status(503).send("503");
+    }
 });
 
 
--- web/public/js/main.js
+++ web/public/js/main.js
@@ -1,1 +1,6 @@
 var app = angular.module('myApp', []);
+
+
+app.controller('statusControler', ['$scope', function($scope) {
+  $scope.greeting = 'Hola!';
+}]);
