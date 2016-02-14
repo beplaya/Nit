ab67f1e23ea7f78164eee9fe84c18257f1a1604a
Grant James <grant.james@cardinalhealth.com>
2016-02-13 21:29:45 -0500

    NIT-11 awesome graphs!

--- team/public/js/controllers/glimrGraphs.js
+++ team/public/js/controllers/glimrGraphs.js
@@ -3,16 +3,70 @@
 angular.module('nitForGitTeamApp').controller('glimrGraphController', ['$scope', 'glimrData',
                                  function($scope, glimrData) {
     $scope.glimrData = glimrData;
+    $scope.max = 15;
     $scope.glimrData.addListener(function(){
-        Highcharts.chart('glimrGraphControllerContainer', {
+        var sprintNames = [];
+        var numberOfCardsMergedArray = [];
+        var numberOfCardsWorkedArray = [];
+        var numberOfCommitsArray = [];
+        var numberOfAuthorsArray = [];
+        var sprints = $scope.glimrData.allSprints.reverse();
+        for(var i=0; (i<sprints.length && i<$scope.max); i++) {
+            sprintNames.push(sprints[i].name);
+            numberOfCardsWorkedArray.push(sprints[i].logsAnalysis.cards.length);
+            numberOfCommitsArray.push(sprints[i].logsAnalysis.logObjects.length);
+            numberOfAuthorsArray.push(sprints[i].logsAnalysis.authors.length);
+            var cardsWorked = sprints[i].logsAnalysis.cards;
+            var numberOfCardsMerged = 0;
+            for(var j=0; j<cardsWorked.length; j++) {
+                var commits = cardsWorked[j].commits;
+                for(var k=0; k<commits.length; k++) {
+                    if(commits[k].pullRequest.isPullRequest) {
+                        numberOfCardsMerged++;
+                        break
+                    }
+                }
+            }
+            numberOfCardsMergedArray.push(numberOfCardsMerged);
+        }
+        sprintNames.reverse();
+        numberOfCardsWorkedArray.reverse();
+        numberOfCardsMergedArray.reverse();
+        numberOfCommitsArray.reverse();
+        numberOfAuthorsArray.reverse();
 
-        xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        },
-
-        series: [{
-            data: [29.9, 71.5, 100*Math.random(), 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-        }]});
-    });
+        var graphData = {
+            title: {text:"GLIMR Sprint Report"},
+            xAxis: {
+                categories: sprintNames
+            },
+            yAxis: [
+                {
+                    gridLineWidth: 1,
+                    title: {
+                        text: '# of Cards'
+                    }
+                },{
+                    gridLineWidth: 1,
+                    title: {
+                        text: '# of Commits'
+                    },
+                    opposite : true
+                },{
+                    gridLineWidth: 1,
+                    title: {
+                        text: '# of Unique Authors'
+                    },
+                    opposite : true
+                }
+            ],
+            series: [
+                { data: numberOfCardsWorkedArray, name:"Cards With Commits", yAxis: 0}
+                ,{ data: numberOfCardsMergedArray, name:"Cards Merged", yAxis: 0}
+                ,{ data: numberOfCommitsArray, name:"Commits", yAxis: 1}
+                ,{ data: numberOfAuthorsArray, name:"Unique Authors", yAxis: 2}
+            ]
+        };
+        Highcharts.chart('glimrGraphControllerContainer', graphData);
+    }, $scope);
 }]);
