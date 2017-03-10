8f7172809e1c60859f5ffd4b154d2779b2fe168f
beplaya <beplaya@gmail.com>
2017-03-10 14:35:23 -0500

    updated glimr to 27

--- team/lib/update.js
+++ team/lib/update.js
@@ -2,7 +2,7 @@ module.exports = function(app, inputReceiver){
     U = {app:app, inputReceiver:inputReceiver};
     U.PSUEDO_SPRINT_LENGTH_DEFAULT = 2*7*24*60*60*1000;//2 weeks
     U.PSUEDO_HISTORY_LENGTH_DEFAULT = 6*4*7*24*60*60*1000;//~ 6 months
-    U.glimr = require(__dirname + '/../node_modules/glimr/glimr.js')();
+    U.glimr = new require(__dirname + '/../node_modules/glimr/glimr.js')();
     U.updatePeriodMin = 45;
     U.init = function() {
         U.inputReceiver.cacheSaver.loadCache();
